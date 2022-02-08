28991c10e8a639a2b2bea036397140090e969bc3
Alexey <kvarel@mail.ru>
2022-02-08 18:03:21 +0300

    hgf

--- spisok_del/settings.py
+++ spisok_del/settings.py
@@ -137,5 +137,3 @@ STATICFILES_DIRS = (
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-django_heroku.settings(locals())
